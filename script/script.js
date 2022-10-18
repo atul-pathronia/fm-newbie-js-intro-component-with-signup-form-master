619cb8a0c6358946f071fa7b581217a462c39b6b
atul_pathronia <patul_25@rocketmail.com>
2022-10-18 21:00:11 +0530

    desktop view

--- script/script.js
+++ script/script.js
@@ -5,32 +5,48 @@ const inputText = document.querySelectorAll("input");
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
+  console.log("clicked");
   inputText.forEach((input) => {
     if (input.name === "fname" && !input.value) {
-      const error = document.createElement("span");
-      error.textContent = "fisrt Name cannot be empty";
-      input.after(error);
+      const errorText = document.createElement("span");
+      errorText.textContent = "fisrt Name cannot be empty";
+      const errorImg = document.createElement("img");
+      errorImg.src = "/images/icon-error.svg";
+      input.after(errorText, errorImg);
+      input.style.borderColor = "#ff7a7a";
     }
     if (input.name === "lname" && !input.value) {
-      const error = document.createElement("span");
-      error.textContent = "Last Name cannot be empty";
-      input.after(error);
+      const errorText = document.createElement("span");
+      errorText.textContent = "Last Name cannot be empty";
+      const errorImg = document.createElement("img");
+      errorImg.src = "/images/icon-error.svg";
+      input.after(errorText, errorImg);
+      input.style.borderColor = "#ff7a7a";
     }
     if (input.name === "email" && !input.value) {
-      const error = document.createElement("span");
-      error.textContent = "Email cannot be empty";
-      input.after(error);
+      const errorText = document.createElement("span");
+      errorText.textContent = "Email cannot be empty";
+      const errorImg = document.createElement("img");
+      errorImg.src = "/images/icon-error.svg";
+      input.after(errorText, errorImg);
+      input.style.borderColor = "#ff7a7a";
     } else if (input.name === "email" && !input.value.includes("@")) {
-      const error = document.createElement("span");
-      error.textContent = "Looks like this is not an email";
-      input.after(error);
+      const errorText = document.createElement("span");
+      errorText.textContent = "Looks like this is not an email";
+      const errorImg = document.createElement("img");
+      errorImg.src = "/images/icon-error.svg";
+      input.after(errorText, errorImg);
       input.value = "";
       input.placeholder = "email@example.com";
+      input.style.borderColor = "#ff7a7a";
     }
     if (input.name === "password" && !input.value) {
-      const error = document.createElement("span");
-      error.textContent = "Password cannot be empty";
-      input.after(error);
+      const errorText = document.createElement("span");
+      errorText.textContent = "Password cannot be empty";
+      const errorImg = document.createElement("img");
+      errorImg.src = "/images/icon-error.svg";
+      input.after(errorText, errorImg);
+      input.style.borderColor = "#ff7a7a";
     }
   });
 });
