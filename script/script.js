112b423ec8af84fcdab7f21a4daf0cabf0416dfb
atul_pathronia <patul_25@rocketmail.com>
2022-10-19 13:04:14 +0530

    functionality enhanced

--- script/script.js
+++ script/script.js
@@ -2,51 +2,65 @@
 
 const submitBtn = document.querySelector(".submitBtn");
 const inputText = document.querySelectorAll("input");
+const firstNameError = document.createElement("span");
+const firstNameErrorImg = document.createElement("img");
+const lastNameError = document.createElement("span");
+const lastNameErrorImg = document.createElement("img");
+const emailError = document.createElement("span");
+const emailErrorImg = document.createElement("img");
+const passwordError = document.createElement("span");
+const passwordErrorImg = document.createElement("img");
 
 submitBtn.addEventListener("click", function (e) {
   e.preventDefault();
-  console.log("clicked");
   inputText.forEach((input) => {
     if (input.name === "fname" && !input.value) {
-      const errorText = document.createElement("span");
-      errorText.textContent = "fisrt Name cannot be empty";
-      const errorImg = document.createElement("img");
-      errorImg.src = "/images/icon-error.svg";
-      input.after(errorText, errorImg);
+      firstNameError.textContent = "First Name cannot be empty";
+      firstNameErrorImg.src = "/images/icon-error.svg";
+      input.after(firstNameError, firstNameErrorImg);
       input.style.borderColor = "#ff7a7a";
+    } else if (input.value) {
+      const firstName = input.value;
+      firstNameError.remove();
+      firstNameErrorImg.remove();
+      input.style.borderColor = "inherit";
     }
     if (input.name === "lname" && !input.value) {
-      const errorText = document.createElement("span");
-      errorText.textContent = "Last Name cannot be empty";
-      const errorImg = document.createElement("img");
-      errorImg.src = "/images/icon-error.svg";
-      input.after(errorText, errorImg);
+      lastNameError.textContent = "Last Name cannot be empty";
+      emailErrorImg.src = "/images/icon-error.svg";
+      input.after(lastNameError, lastNameErrorImg);
       input.style.borderColor = "#ff7a7a";
+    } else if (input.value) {
+      lastNameError.remove();
+      lastNameErrorImg.remove();
+      input.style.borderColor = "inherit";
     }
     if (input.name === "email" && !input.value) {
-      const errorText = document.createElement("span");
-      errorText.textContent = "Email cannot be empty";
-      const errorImg = document.createElement("img");
-      errorImg.src = "/images/icon-error.svg";
-      input.after(errorText, errorImg);
+      emailError.textContent = "Email cannot be empty";
+      emailErrorImg.src = "/images/icon-error.svg";
+      input.after(emailError, emailErrorImg);
       input.style.borderColor = "#ff7a7a";
     } else if (input.name === "email" && !input.value.includes("@")) {
-      const errorText = document.createElement("span");
-      errorText.textContent = "Looks like this is not an email";
-      const errorImg = document.createElement("img");
-      errorImg.src = "/images/icon-error.svg";
-      input.after(errorText, errorImg);
+      emailError.textContent = "Looks like this is not an email";
+      emailErrorImg.src = "/images/icon-error.svg";
+      input.after(emailError, emailErrorImg);
       input.value = "";
       input.placeholder = "email@example.com";
       input.style.borderColor = "#ff7a7a";
+    } else if (input.value) {
+      emailError.remove();
+      emailErrorImg.remove();
+      input.style.borderColor = "inherit";
     }
     if (input.name === "password" && !input.value) {
-      const errorText = document.createElement("span");
-      errorText.textContent = "Password cannot be empty";
-      const errorImg = document.createElement("img");
-      errorImg.src = "/images/icon-error.svg";
-      input.after(errorText, errorImg);
+      passwordError.textContent = "Password cannot be empty";
+      passwordErrorImg.src = "/images/icon-error.svg";
+      input.after(passwordError, passwordErrorImg);
       input.style.borderColor = "#ff7a7a";
+    } else if (input.value) {
+      passwordError.remove();
+      passwordErrorImg.remove();
+      input.style.borderColor = "inherit";
     }
   });
 });
